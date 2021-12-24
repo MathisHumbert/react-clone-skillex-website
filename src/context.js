1c710450968d41a738dd42dd33f6edb3d59589a3
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-12-24 10:01:46 +0100

    Context API created

--- src/context.js
+++ src/context.js
@@ -3,11 +3,16 @@ import React, { useState, useContext } from 'react';
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppProvider value="data">{children}</AppProvider>;
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  return (
+    <AppContext.Provider value={{ isSidebarOpen }}>
+      {children}
+    </AppContext.Provider>
+  );
 };
 
 const useGlobalContext = () => {
   return useContext(AppContext);
 };
-
 export { AppContext, AppProvider, useGlobalContext };
